f444354e3b292f10499c27661a8edb9610af6c76
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-12-30 12:44:32 -0600

    superposition working properly

--- src/gates.go
+++ src/gates.go
@@ -23,9 +23,9 @@ func pauliXGate(dim int) [][]complex128 {
     return gate
 }
 
-func fourierTransformGate(dim int) [][]complex128 {
+func hadamardGate(dim int) [][]complex128 { // same as fourier transform gate; superimposes or undoes superposition
     gate := make([][]complex128, dim)
-    omega := cmplx.Exp(2 * math.Pi * 1i / complex(float64(dim), 0))
+    omega := cmplx.Exp(2 * math.Pi * 1i / complex(float64(dim), 0)) // primitive root of unity
     for i := 0; i < dim; i++ {
         gate[i] = make([]complex128, dim)
         for j := 0; j < dim; j++ {
@@ -33,4 +33,22 @@ func fourierTransformGate(dim int) [][]complex128 {
         }
     }
     return gate
-} 
+}
+
+func cNotGate(dim int) [][]complex128 {
+    gate := make([][]complex128, dim*dim) // Create a 2D matrix of size d^2 for the control-target qudit pair
+    // Initialize the matrix elements to 0
+    for i := 0; i < dim*dim; i++ {
+        gate[i] = make([]complex128, dim*dim)
+    }
+    // Identity operation when control qudit is in state |0>
+    for i := 0; i < dim; i++ {
+        gate[i][i] = 1
+    }
+    // Cyclic permutation when control qudit is in state |1>
+    for i := 0; i < dim-1; i++ {
+        gate[dim+i][dim+i+1] = 1
+    }
+    gate[2*dim-1][dim] = 1 // Circular permutation for last state
+    return gate
+} // TODO: Test this function
--- src/main.go
+++ src/main.go
@@ -10,7 +10,7 @@ func main() {
         fmt.Println("src/main.go : main() :: ERROR ::: Failed to initialize qudit0")
         return
     }
-    qudit1, err := singletonQudit([]complex128{0, -1})
+    qudit1, err := singletonQudit([]complex128{0, 0, -1i})
     if err != nil {
         fmt.Println("src/main.go : main() :: ERROR ::: Failed to initialize qudit1")
         return
@@ -23,37 +23,37 @@ func main() {
         fmt.Println("src/main.go : main() :: ERROR ::: Tensor product failed. Quantum register is empty.")
         return
     }
-    fmt.Println("\nsrc/main.go : main() :: Tensor Product of Quantum Register before pauliXGate:", jointState.Amplitudes)
+    fmt.Println("\nsrc/main.go : main() :: Tensor Product of Quantum Register before Hadamard Gate:", jointState.Amplitudes)
     qrCounts := make(map[string]int)
     for i := 0; i < ITERS; i++ {
         measurement := register.measure(getObservation())
         stateKey := fmt.Sprint(measurement)
         qrCounts[stateKey]++
     }
-    fmt.Printf("\nsrc/main.go : main() :: Quantum Register before pauliXGate results after %d iterations :::\n", ITERS)
+    fmt.Printf("\nsrc/main.go : main() :: Quantum Register before Hadamard Gate results after %d iterations :::\n", ITERS)
     for state, count := range qrCounts {
         fmt.Printf("\t -> Quantum Register State %s: %d occurrences\n", state, count)
     }
-    fmt.Println("\nsrc/main.go : main() :: qudit0 before pauliXGate:", register.Qudits[0].Amplitudes)
-    xgate := pauliXGate(3)
-    if err := register.applyGateToQudit(0, xgate); err != nil {
+    fmt.Println("\nsrc/main.go : main() :: qudit0 before Hadamard Gate:", register.Qudits[0].Amplitudes)
+    hgate := hadamardGate(3)
+    if err := register.applyGateToQudit(0, hgate); err != nil {
         fmt.Println(err)
         return
     }
-    fmt.Println("src/main.go : main() :: qudit0 after pauliXGate:", register.Qudits[0].Amplitudes)
+    fmt.Println("src/main.go : main() :: qudit0 after Hadamard Gate:", register.Qudits[0].Amplitudes)
     jointState = register.TensorProduct()
     if jointState == nil {
         fmt.Println("src/main.go : main() :: ERROR ::: Tensor product failed. Quantum register is empty.")
         return
     }
-    fmt.Println("\nsrc/main.go : main() :: Tensor Product of Quantum Register after pauliXGate:", jointState.Amplitudes)
+    fmt.Println("\nsrc/main.go : main() :: Tensor Product of Quantum Register after Hadamard Gate:", jointState.Amplitudes)
     qrCounts = make(map[string]int)
     for i := 0; i < ITERS; i++ {
         measurement := register.measure(getObservation())
         stateKey := fmt.Sprint(measurement)
         qrCounts[stateKey]++
     }
-    fmt.Printf("\nsrc/main.go : main() :: Quantum Register after pauliXGate results after %d iterations :::\n", ITERS)
+    fmt.Printf("\nsrc/main.go : main() :: Quantum Register after Hadamard Gate results after %d iterations :::\n", ITERS)
     for state, count := range qrCounts {
         fmt.Printf("\t -> Quantum Register State %s: %d occurrences\n", state, count)
     }
